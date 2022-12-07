6ac943d8be805b95b40d70d644aecd13c8bf9e2b
Cosa Andrei-Rares <64634481+Rares-Cosa@users.noreply.github.com>
2022-12-07 21:44:28 +0200

    display x on a clicked cell

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
+import { useState } from "react";
 import './App.css';
+import _ from "lodash";
+
 
 function App() {
+  const [xoState, setXoState] = useState([  // Rember the state of the game board
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ]);
+
+
+  const click = (row, column) => {  // For now save X on a clicked cell
+    let newXoState = _.cloneDeep(xoState);
+    newXoState[row][column] = "X";
+    setXoState(newXoState);
+  };
+
+  
   const generateCells = () => {
     let cellsArray = [];
 
-    for (let row = 0; row < 3; ++row) {  // Generate 9 cells
+    for (let row = 0; row < 3; ++row) { // Generate 9 cells
       for (let column = 0; column < 3; ++column) {
         cellsArray.push(
           <div 
             className="cell" 
-            style={{ gridColumnStart: column + 1, gridRowStart: row + 1 }} 
-          /> 
+            style={{ gridColumnStart: column + 1, gridRowStart: row + 1 }}
+            onClick={() => click(row, column)}
+          >
+            {xoState[row][column]}
+          </div>  
         )
       }
     }
